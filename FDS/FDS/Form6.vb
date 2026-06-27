53594dca365b6421fd35ed49a0ecc6b6be010fa0
Ivan Capanay <123456789+navybye15@users.noreply.github.com>
2026-06-27 18:24:24 +0800

    Update Form6

--- FDS/FDS/Form6.vb
+++ FDS/FDS/Form6.vb
@@ -1,6 +1,14 @@
 ﻿Public Class Form6
 
     Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
 
     End Sub
 
@@ -11,6 +19,24 @@
     End Sub
 
     Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs) Handles Panel3.Paint
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+        Form7.Show()
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+        Form8.Show()
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles Label13.Click
+        Form9.Show()
+        Me.Hide()
 
     End Sub
 End Class
