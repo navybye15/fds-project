4b6a804b2e1b469695a88ce125063281d0109bb8
Aly <oardealyssamay@gmail.com>
2026-07-12 21:12:55 +0800

    added sign out in form 12

--- FDS/FDS/Form12.vb
+++ FDS/FDS/Form12.vb
@@ -281,4 +281,8 @@ Public Class Form12
         Form19.RefreshAndShow()
         Me.Hide()
     End Sub
+
+    Private Sub btnSignOut_Click(sender As Object, e As EventArgs) Handles btnSignOut.Click
+        Session.SignOut(Me)
+    End Sub
 End Class
